38501ca392f1f7e9d2a79da22a0d2a7f51480154
Julien Castelain <jcastelain@gmail.com>
2013-06-22 16:03:03 +0200

    adding vec2 and tests

---
+++ vec2_test.lua
@@ -0,0 +1,47 @@
+--[[ Tests for Vec2  ]]--
+
+package.path = './?.lua;' .. package.path
+require 'vec2'
+
+-- new
+v = Vec2:new({x = 10 , y = 10})
+assert(v.x == 10)
+assert(v.y == 10)
+print('x: ' .. v.x .. ' y: ' .. v.y)
+
+-- add
+v:add({x = 10, y = 10})
+assert(v.x == 20)
+assert(v.y == 20)
+print('x: ' .. v.x .. ' y: ' .. v.y)
+
+-- sub
+v:sub({x = 10, y = 10})
+assert(v.x == 10)
+assert(v.y == 10)
+print('x: ' .. v.x .. ' y: ' .. v.y)
+
+-- mult
+v:mult(2)
+assert(v.x == 20)
+assert(v.y == 20)
+print('x: ' .. v.x .. ' y: ' .. v.y)
+
+-- div
+v:div(2)
+assert(v.x == 10)
+assert(v.y == 10)
+print('x: ' .. v.x .. ' y: ' .. v.y)
+
+-- mag
+assert(v:mag() == math.sqrt(10 * 10 + 10 * 10))
+
+-- maq_sq
+assert(v:mag_sq() == (10 * 10 + 10 * 10))
+
+-- equals
+assert(v:equals({x = 10, y = 10}) == true)
+
+-- more to come ...
+-- it's siesta time
+
